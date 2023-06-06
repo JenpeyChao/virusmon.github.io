15b8f81bfe49bd7b1adfbd9343296b03e20298f6
aaron <redskin.rox@gmail.com>
2023-06-06 19:50:57 -0400

    updated url

--- js/results.js
+++ js/results.js
@@ -99,7 +99,7 @@ function init_pokedex(file){
 async function analyzeHash(hash_input)
 {
     console.log(hash_input)
-    await fetch(`http://localhost:3000/search/${hash_input}`)
+    await fetch(`https://virusmon.onrender.com/search/${hash_input}`)
     .then(response => {
         if(response.ok) {
             return response.json()
